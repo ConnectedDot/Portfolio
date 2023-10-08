b2f85278b4b2c9e723a0709cf33da98786e7178c
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-10-08 06:33:38 +0100

    Working the portfolio

---
+++ src/content_option.js
@@ -0,0 +1,311 @@
+import logo from "../src/assets/20220502_150835.jpg";
+import port1 from "../src/assets/images/port1.jpg";
+
+const logotext = "THEOPHILUS";
+const logobrand = "ConnectedDot";
+const meta = {
+  title: "  Theophilus Samuel",
+  description:
+    "I’m Theophilus Samuel, an IT Specialist and a Full stack devloper, currently working at Lotus Beta Analytics",
+};
+
+const introdata = {
+  title: "I’m Theophilus Samuel",
+  animated: {
+    first: "I love coding",
+    second: "I design cool websites",
+    third: "I develop responsive front-ends",
+    fourth: "I'm very much part and always part of the Open Source",
+  },
+  description:
+    "I'm a Research Manager, providing digital, marketing and media support to businesses (both P2P and B2B). Also an accelerator, responsible for some of Harambee's key learning and knowledge streams, that includes providing education, opportunities for youth in the space of micro-technology. Also an economist & policy strategist passionate about optimizing the conditions for human thriving. My work focuses on understanding the policy building blocks and helping Startups solve complex problems. To cap, I have more than 5 Year experience as a media personnel who featured for multiple projects for brands and Startups in boosting online presence, video-conferencing and webinars.",
+  your_img_url: { logo },
+};
+
+const dataabout = {
+  title: "A bit about myself",
+  aboutme:
+    "I'm a Research Manager, providing digital, marketing and media support to businesses (both P2P and B2B). Also an accelerator, responsible for some of Harambee's key learning and knowledge streams, that includes providing education, opportunities for youth in the space of micro-technology. Also an economist & policy strategist passionate about optimizing the conditions for human thriving. My work focuses on understanding the policy building blocks and helping Startups solve complex problems. To cap, I have more than 5 Year experience as a media personnel who featured for multiple projects for brands and Startups in boosting online presence, video-conferencing and webinars.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Git & GitHub",
+    value: 96,
+  },
+  {
+    name: "Wordpress Development",
+    value: 95,
+  },
+  {
+    name: "HTML & CSS",
+    value: 90,
+  },
+  {
+    name: "Adobe Photoshop",
+    value: 88,
+  },
+  {
+    name: "Node JS",
+    value: 85,
+  },
+  {
+    name: "React JS",
+    value: 85,
+  },
+  {
+    name: "MySQL",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "Advanced CorelDraw",
+    value: 78,
+  },
+  {
+    name: "Python",
+    value: 60,
+  },
+  {
+    name: "Typescript",
+    value: 60,
+  },
+];
+
+const services = [
+  {
+    title: "Graphic Design",
+    description:
+      "With my extensive knowledge and skills in graphic design, I have the ability to create eye-catching visuals that effectively communicate your message and captivate your target audience. Whether you need a logo, brochure, poster, or any other design element, I can offer creative solutions tailored to your specific needs. Through thoughtful use of color, typography, and imagery, I strive to create designs that not only look visually appealing but also convey your brand identity and values.",
+  },
+  {
+    title: "Web Designs",
+    description:
+      "As an experienced web designer, I possess a deep understanding of user experience (UX) principles and can translate your vision into a seamless and engaging online presence. By conducting thorough research and analysis of your industry and target audience, I ensure that your website is not only aesthetically pleasing but also user-friendly and intuitive. From layout and navigation to responsiveness across different devices, I pay meticulous attention to detail to deliver a website that leaves a lasting positive impression on visitors.",
+  },
+  {
+    title: "Wordpress Design",
+    description:
+      "my WordPress design services encompass creating custom themes, modifying existing ones, ensuring responsive design, integrating e-commerce solutions and plugins, and providing ongoing maintenance and support.",
+  },
+  {
+    title: "IT Specialization",
+    description:
+      "As an experienced IT specialist, I possess a comprehensive understanding of various systems, networks, and technologies. Whether you're facing hardware or software issues, network connectivity problems, or data security concerns, I am well-equipped to provide expert guidance and troubleshooting. With my meticulous attention to detail and dedication to problem-solving, I ensure that your technical challenges are addressed efficiently and effectively, minimizing downtime and disruption to your operations.",
+  },
+  {
+    title: "Social Media Management",
+    description:
+      "In the ever-evolving landscape of social media, I have honed my expertise in managing online platforms to help businesses and individuals thrive. From creating engaging content to implementing targeted advertising campaigns, I take a strategic approach to maximize your visibility, reach, and engagement on various social media platforms. With a deep understanding of analytics and trends, I continuously monitor and optimize your social media presence, ensuring that your brand stays relevant and connected with your audience.",
+  },
+  {
+    title: "Media Livestreaming",
+    description:
+      "With my advanced capabilities in media livestreaming, I offer a comprehensive solution for broadcasting events to a global audience. Using state-of-the-art equipment and technologies, I enable high-quality live streaming with minimal latency, ensuring that viewers feel immersed in the action. Whether it's a conference, concert, sporting event, or any other live occasion, I provide seamless integration of video, audio, and interactive features, allowing attendees to participate and engage from anywhere in the world.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: { port1 },
+    description:
+      "Here is a Attendance Management System using QR Code and Face Scanner.",
+    link: "https://aaranoams.azurewebsites.net/",
+  },
+  {
+    img: { port1 },
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://my-pokemons.netlify.app/",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+const datacontributions = [
+  {
+    img: { port1 },
+    description:
+      "Here is a Attendance Management System using QR Code and Face Scanner.",
+    link: "https://aaranoams.azurewebsites.net/",
+  },
+  {
+    img: { port1 },
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://my-pokemons.netlify.app/",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "name@domain.com",
+  YOUR_FONE: "(555)123-4567",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/ConnectedDot",
+  facebook: "https://www.facebook.com/TheophilusSams?mibextid=ZbWKwL",
+  linkedin: "https://www.linkedin.com/in/theophilus-mcsamuel-6108091a6",
+  twitter: "https://twitter.com/mc_samuell.com",
+  instagram: "https://instagram.com/theophilus_mcsam?igshid=NGExMmI2YTkyZg",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  datacontributions,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+  logobrand,
+};
